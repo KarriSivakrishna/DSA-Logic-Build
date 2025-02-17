392c2e8909965c1a2d9268fa9da16c7a13859bf5
KARRI SIVAKRISHNA <119671183+KarriSivakrishna@users.noreply.github.com>
2025-02-17 06:20:05 +0530

    Time: 3 ms (10.71%), Space: 17.7 MB (82.12%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -3,8 +3,8 @@ class Solution:
         jewel_set = set(jewels)  
         count = 0
         
-        for stone in stones:
-            if stone in jewel_set:
+        for i in stones:
+            if i in jewel_set:
                 count += 1
                 
         return count
