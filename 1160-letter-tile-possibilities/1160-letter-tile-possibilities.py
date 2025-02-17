15447487665423716c13995f09a32bebc9830d32
KARRI SIVAKRISHNA <119671183+KarriSivakrishna@users.noreply.github.com>
2025-02-17 06:16:14 +0530

    Time: 27 ms (76.29%), Space: 19.5 MB (26.97%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -0,0 +1,6 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        res = {''}
+        for c in tiles:
+            res |= {d[:i] + c + d[i:] for d in res for i in range(len(d) + 1)}
+        return len(res) - 1
