ae1befb910f4565190f74ef44384addf2d672516
KARRI SIVAKRISHNA <119671183+KarriSivakrishna@users.noreply.github.com>
2025-07-05 08:09:22 +0530

    Time: 5 ms (11.04%), Space: 17.8 MB (50.63%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
         c=Counter(arr)
-        maxi=-1
+        result=-1
         for i in arr:
             if i==c[i]:
-                maxi=max(maxi,i)
-        return maxi
+                result=max(result,i)
+        return result
